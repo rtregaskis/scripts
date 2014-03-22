598f4c9842c02e321ab4f089008d5f75ca06f099
rtregaskis <rtregaskis@dresources.com>
2014-03-22 14:18:23 +0000

    use cammand line args for files to convert. if no args, warn and exit

--- fix-html-entity.sh
+++ fix-html-entity.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+if [ $# == 0 ]; then
+	echo "No args passed!";
+	echo "script requires a path to files to operate upon"
+	exit -1
+fi
+
 # read in file, convertdefined set of characters to corresponding html entity tags
 #NB: uses perl to do replacement as Mac OSX sed is broken...
-REFS=assets/data/references/*.json
+FILES=("$@")
 
-for ref in $REFS; do
-	perl -pi -e 's/®/&reg;/g' $ref
-	perl -pi -e 's/£/&pound;/g' $ref
+for file in $FILES; do
+	perl -pi -e 's/®/&reg;/g' $file
+	perl -pi -e 's/£/&pound;/g' $file
 done
