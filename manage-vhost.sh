f53fa1170cc3c8fcfaadf7cd61dbe41d0ffa07dd
rtregaskis <rtregaskis@dresources.com>
2014-04-30 11:43:03 +0100

    add new `restore` option, replace config files with factory defaults. addHosts how calls cleanVhosts which removes example vhosts

--- manage-vhost.sh
+++ manage-vhost.sh
@@ -27,8 +27,12 @@ fi
 
 ##
 # Get a timestamp
-function timetamp {
-	TS=date +"%Y%m%d%H%M%S"
+# usage:
+# TS=$(timestamp)
+# echo $TS
+function timestamp() {
+	local __result=`date +"%Y%m%d-%H%M%S"`
+	echo "$__result"
 }
 
 ##
@@ -41,11 +45,21 @@ function usage {
 	echo "-p <port> : port number to access site by"
 	echo "-d <path> : top-level directory of website"
 	echo "-r <port> : remove virtual host on this port"
+	echo "-R : restore config files to defaults"
 	echo "NB: arguments can be given in any order."
 	echo ""
 	echo "Run with no arguments for interactive mode"
 	echo "example: manage-vhost -n mysite -p 9999 -d ~/path/to/site/"
 	echo "On completion, the site will be available at localhost:9999"
+}
+
+##
+# Clean up vhosts file, remove dummy entries
+function cleanupVhosts() {
+	cp $VHOSTS $VHOSTS.backup
+
+	sed -i '' "/NameVirtualHost \*:80/d" $VHOSTS
+	sed -i '' "/\<VirtualHost \*:80\>/,/\<\/VirtualHost\>/d" $VHOSTS
 }
 
 ##
@@ -83,7 +97,7 @@ function addHost {
 	if [ ! -f /etc/apache2/httpd.conf.abacus ]; then
 		echo "Back up httpd.conf and enable virtual hosts"
 		cp /etc/apache2/httpd.conf /etc/apache2/httpd.conf.abacus
-		sed -i '' -e "s/#Include \/private\/etc\/apache2\/extra\/httpd-vhosts.conf/Include \/private\/etc\/apache2\/extra\/httpd-vhosts.conf/" /etc/apache2/httpd.conf
+		sed -i '' "s/#Include \/private\/etc\/apache2\/extra\/httpd-vhosts.conf/Include \/private\/etc\/apache2\/extra\/httpd-vhosts.conf/" /etc/apache2/httpd.conf
 	fi
 
 	#Create httpd-vhosts.conf(if necessary)
@@ -91,6 +105,9 @@ function addHost {
 		echo "Create $VHOSTS"
 		touch $VHOSTS
 	fi
+
+	# backup original vhost file, remove dummy entries
+	cleanupVhosts
 
 	echo "Append site details to $VHOSTS"
 	echo "# Virtual host start $NAME $PORT" >> $VHOSTS
@@ -165,7 +182,6 @@ function removeHost {
 	do           
 		if [[ $LINE == *Virtual\ host\ start* ]]
 		then
-			echo "here ${LINE:(-4)}"
 			if [[ ${LINE:(-4)} == $PORT ]]
 			then
 				CAPTURE=0
@@ -196,6 +212,75 @@ function removeHost {
 	done 
 }
 
+function restore() {
+	echo "Restore $VHOSTS and $HOSTS to defaults."
+	if [ ! -f $VHOSTS.modified ]; then
+		cp $VHOSTS $VHOSTS.modified
+	fi
+
+	if [ ! -f $HOSTS.modified ]; then
+		cp $HOSTS $HOSTS.modified
+	fi
+	cp /etc/apache2/httpd.conf.abacus /etc/apache2/httpd.conf
+
+	echo '#' > $VHOSTS
+	echo '# Virtual Hosts' >> $VHOSTS
+	echo '#' >> $VHOSTS
+	echo '# If you want to maintain multiple domains/hostnames on your' >> $VHOSTS
+	echo '# machine you can setup VirtualHost containers for them. Most configurations' >> $VHOSTS
+	echo "# use only name-based virtual hosts so the server doesn't need to worry about" >> $VHOSTS
+	echo '# IP addresses. This is indicated by the asterisks in the directives below.' >> $VHOSTS
+	echo '#' >> $VHOSTS
+	echo '# Please see the documentation at ' >> $VHOSTS
+	echo '# <URL:http://httpd.apache.org/docs/2.2/vhosts/>' >> $VHOSTS
+	echo '# for further details before you try to setup virtual hosts.' >> $VHOSTS
+	echo '#' >> $VHOSTS
+	echo '# You may use the command line option '-S' to verify your virtual host' >> $VHOSTS
+	echo '# configuration.' >> $VHOSTS
+	echo '' >> $VHOSTS
+	echo '#' >> $VHOSTS
+	echo '# Use name-based virtual hosting.' >> $VHOSTS
+	echo '#' >> $VHOSTS
+	echo 'NameVirtualHost *:80' >> $VHOSTS
+	echo '' >> $VHOSTS
+	echo '#' >> $VHOSTS
+	echo '# VirtualHost example:' >> $VHOSTS
+	echo '# Almost any Apache directive may go into a VirtualHost container.' >> $VHOSTS
+	echo '# The first VirtualHost section is used for all requests that do not' >> $VHOSTS
+	echo '# match a ServerName or ServerAlias in any <VirtualHost> block.' >> $VHOSTS
+	echo '#' >> $VHOSTS
+	echo '<VirtualHost *:80>' >> $VHOSTS
+	echo '    ServerAdmin webmaster@dummy-host.example.com' >> $VHOSTS
+	echo '    DocumentRoot "/usr/docs/dummy-host.example.com"' >> $VHOSTS
+	echo '    ServerName dummy-host.example.com' >> $VHOSTS
+	echo '    ServerAlias www.dummy-host.example.com' >> $VHOSTS
+	echo '    ErrorLog "/private/var/log/apache2/dummy-host.example.com-error_log"' >> $VHOSTS
+	echo '    CustomLog "/private/var/log/apache2/dummy-host.example.com-access_log" common' >> $VHOSTS
+	echo '</VirtualHost>' >> $VHOSTS
+	echo '' >> $VHOSTS
+	echo '<VirtualHost *:80>' >> $VHOSTS
+	echo '    ServerAdmin webmaster@dummy-host2.example.com' >> $VHOSTS
+	echo '    DocumentRoot "/usr/docs/dummy-host2.example.com"' >> $VHOSTS
+	echo '    ServerName dummy-host2.example.com' >> $VHOSTS
+	echo '    ErrorLog "/private/var/log/apache2/dummy-host2.example.com-error_log"' >> $VHOSTS
+	echo '    CustomLog "/private/var/log/apache2/dummy-host2.example.com-access_log" common' >> $VHOSTS
+	echo '</VirtualHost>' >> $VHOSTS
+	echo '' >> $VHOSTS
+
+	echo "##" > $HOSTS
+	echo "# Host Database" >> $HOSTS
+	echo "#" >> $HOSTS
+	echo "# localhost is used to configure the loopback interface" >> $HOSTS
+	echo "# when the system is booting.  Do not change this entry." >> $HOSTS
+	echo "##" >> $HOSTS
+	echo "127.0.0.1	localhost" >> $HOSTS
+	echo "255.255.255.255	broadcasthost" >> $HOSTS
+	echo "::1             localhost" >> $HOSTS
+	echo "fe80::1%lo0	localhost" >> $HOSTS
+	echo "" >> $HOSTS
+
+}
+
 #if no args passed, whinge and exit
 if [ $# == 0 ]; then
 	echo "This script will update your apache configuration to allow websites to be addressed by port number."
@@ -212,7 +297,7 @@ fi
 # if there are cmd line args, parse them
 if [ $ARGS_OK == 0 ]; then
 	#parse arguments
-	while getopts "lhp:d:n:r:" opt; do
+	while getopts "lhRp:d:n:r:" opt; do
 	  case $opt in
 		r)
 		  #remove host by port number
@@ -243,6 +328,11 @@ if [ $ARGS_OK == 0 ]; then
 		  ;;
 		l)
 			listHosts
+			exit 0
+			;;
+		R)
+			restore
+		  	apachectl restart
 			exit 0
 			;;
 		h)
